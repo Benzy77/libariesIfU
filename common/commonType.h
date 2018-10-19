91a88fc9e854cce8baea85c21a4ff38920d2142a
Ben <remember7study520@163.com>
2018-10-20 00:14:31 +0800

    [task]creat a new common type for this project

---
+++ common/commonType.h
@@ -0,0 +1,30 @@
+/*******************************************************************************
+*  Copyleft (C), 2018,
+*  FileName:      commonType.h
+*  Author:        benqiang.yu
+*  Version :      1.0
+*  Date:          2018-10-15
+*  History:
+*      <author>       <time>        <version >      <desc>
+*    benqiang.yu      2018-10-15        1.0           create
+*
+*  Description:   Assert. and commonType creat.
+*  This is a creat a new  common type for this project.
+*******************************************************************************/
+#ifndef COMMON_TYPE_DEFIN_H
+#define COMMON_TYPE_DEFIN_H
+
+typedef unsigned char      U8;
+typedef short              S16;
+typedef unsigned short     U16;
+typedef int                Int32;
+typedef unsigned int       U32;
+typedef long long          Int64;
+
+#define COMMONTYPE_STATUS_SUCCESS      (0)
+#define COMMONTYPE_STATUS_ERROR        (-1)
+#define COMMONTYPE_STATUS_ARG_INVALID  (-2)
+
+
+#endif/* COMMON_TYPE_DEFIN_H */
+/* end of file*/
